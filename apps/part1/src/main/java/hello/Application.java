7bcbf2d4163a49cfcdaea95b218e0ab2da77eb5d
exabugs <exabugs@gmail.com>
2023-06-03 03:13:53 +0000

    add @RequestMapping("/hey")

--- apps/part1/src/main/java/hello/Application.java
+++ apps/part1/src/main/java/hello/Application.java
@@ -14,6 +14,11 @@ public class Application {
 		return "Hello Docker World";
 	}
 
+	@RequestMapping("/hey")
+	public String hey() {
+		return "Hey Yoh";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
